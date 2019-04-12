ea4c198e7b0810f9e4a429bfea76b612ec8129ab
Alan Wong <captainalan@gmail.com>
2019-04-12 09:48:25 -0700

    some code refactoring and a visual update

--- calculator.js
+++ calculator.js
@@ -1,124 +1,123 @@
+var current_input = [];
+var calculator = new MathSolver
+
+var RESULT_MSG = "Enter an expression and then press the '=' button (or hit ENTER on your keyboard) to calculate result";
+var INPUT_MSG  = "Example: 1 1 +";
+
 $(document).ready(function() {
-  
-  var current_input = [];
-  var calculator = new MathSolver
+  // Clean game state
+  clear();
+});
 
-  var RESULT_MSG = "Enter an expression and then press the '=' button (or hit ENTER on your keyboard) to calculate result";
-  var INPUT_MSG  = "Example: 1 1 +";
+function refreshDisplay() {
+  current_input !== "" 
+    ? $('#display').text(current_input.join('')) 
+    : $('#display').text("No input yet.");
+}
 
-  $('#display').text(INPUT_MSG);
-  $('#result').text(RESULT_MSG);
+function pushSymbol(x) {
+  current_input.push(x);
+  refreshDisplay();
+}
 
-  function refreshDisplay() {
-    current_input !== "" 
-      ? $('#display').text(current_input.join('')) 
-      : $('#display').text("No input yet.");
-  }
+function popSymbol() {
+  current_input.pop();
+  refreshDisplay();
+}
 
-  function pushSymbol(x) {
-    current_input.push(x);
-    refreshDisplay();
-  }
+function evalResult() {
+  if (current_input.length > 0)
+    $('#result').text(calculator.solvePostfix(current_input.join('')));
+  refreshDisplay();
+} 
 
-  function popSymbol() {
-    current_input.pop();
-    refreshDisplay();
-  }
+function clear(){
+  current_input = [];
+  refreshDisplay();
+  $('#display').html('<span class="greyed_out">' + INPUT_MSG + '</span>');
+  $('#result').html('<span class="greyed_out">' + RESULT_MSG + '</span>');
+}
 
-  function evalResult() {
-    if (current_input.length > 0)
-      $('#result').text(calculator.solvePostfix(current_input.join('')));
-    refreshDisplay();
-  } 
+/* Number buttons */
+$('.number').on("click", function() {
+  pushSymbol($(this).attr("value"))
+});
 
-  function clear(){
-    current_input = [];
-    refreshDisplay();
-    $('#display').text(INPUT_MSG);
-    $('#result').text(RESULT_MSG);
-  }
+/* Operators */
+$('.operator').on("click", function() {
+  pushSymbol($(this).attr("value"))
+});
 
-  /* Number buttons */
-  $('.number').on("click", function() {
-    pushSymbol($(this).attr("value"))
-  });
-  
-  /* Operators */
-  $('.operator').on("click", function() {
-    pushSymbol($(this).attr("value"))
-  });
+/* Other buttons */
+$('#button-equal').on("click", function() {
+  evalResult();
+});
 
-  /* Other buttons */
-  $('#button-equal').on("click", function() {
+$('#button-space').on("click", function() {
+  pushSymbol(' ');
+});
+
+$('#button-clear').on("click", function() {
+  clear();
+});
+
+
+/* Keyboard Control */
+var validKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','^', ' ']
+$(document).keyup(function(event) {
+
+  var isValid;
+  (validKeys.indexOf(event.key) !== -1) ? isValid = true : isValid = false;
+
+  if (event.key === 'Enter') {
     evalResult();
-  });
-
-  $('#button-space').on("click", function() {
-    pushSymbol(' ');
-  });
-
-  $('#button-clear').on("click", function() {
-    clear();
-  });
-
-
-  /* Keyboard Control */
-  var validKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','^', ' ']
-  $(document).keyup(function(event) {
-
-    var isValid;
-    (validKeys.indexOf(event.key) !== -1) ? isValid = true : isValid = false;
-
-    if (event.key === 'Enter') {
-      evalResult();
-    } else if (event.key === "Backspace" || event.key === "Delete") {
-      popSymbol();
-    } else if (isValid) {
-      pushSymbol(event.key);
-    } else if (event.key === 'c') {
-      clear(); // Do nothing for invalid keyboard input
-    }
-  });
+  } else if (event.key === "Backspace" || event.key === "Delete") {
+    popSymbol();
+  } else if (isValid) {
+    pushSymbol(event.key);
+  } else if (event.key === 'c') {
+    clear(); // Do nothing for invalid keyboard input
+  }
+});
 
 
-  function MathSolver() {
-    this.solvePostfix = function(postfix) {
-      var resultStack = []; // Store a list of tokens 
-      postfix = postfix.split(" ");
-      for(var i = 0; i < postfix.length; i++) {
-        if(postfix[i].isNumeric()){
-          resultStack.push(postfix[i]);
-        } else {
-          var a = resultStack.pop();
-          var b = resultStack.pop();
-          switch(postfix[i]){
-            case "+":
-              resultStack.push(parseInt(a) + parseInt(b));
-              break;
-            case "-":
-              resultStack.push(parseInt(a) - parseInt(b));
-              break;
-            case "*":
-              resultStack.push(parseInt(a) * parseInt(b));
-              break;
-            case "/":
-              resultStack.push(parseInt(a) / parseInt(b));
-              break;
-            case "^":
-              resultStack.push(Math.pow(parseInt(b), parseInt(a)));
-              break;
-          }
+function MathSolver() {
+  this.solvePostfix = function(postfix) {
+    var resultStack = []; // Store a list of tokens 
+    postfix = postfix.split(" ");
+    for(var i = 0; i < postfix.length; i++) {
+      if(postfix[i].isNumeric()){
+        resultStack.push(postfix[i]);
+      } else {
+        var a = resultStack.pop();
+        var b = resultStack.pop();
+        switch(postfix[i]){
+          case "+":
+            resultStack.push(parseInt(a) + parseInt(b));
+            break;
+          case "-":
+            resultStack.push(parseInt(a) - parseInt(b));
+            break;
+          case "*":
+            resultStack.push(parseInt(a) * parseInt(b));
+            break;
+          case "/":
+            resultStack.push(parseInt(a) / parseInt(b));
+            break;
+          case "^":
+            resultStack.push(Math.pow(parseInt(b), parseInt(a)));
+            break;
         }
       }
-      if(resultStack.length > 1) {
-        return "error";
-      } else {
-        return resultStack.pop();
-      }
+    }
+    if(resultStack.length > 1) {
+      return "error";
+    } else {
+      return resultStack.pop();
     }
   }
+}
 
-});
 
 String.prototype.isNumeric = function() {
   return !isNaN(parseFloat(this)) && isFinite(this);
